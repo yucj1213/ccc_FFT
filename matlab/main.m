2b527940202f47f517417c0a2ec528e68609b6a9
huanggang555 <116189259+huanggang555@users.noreply.github.com>
2023-07-07 09:52:21 +0800

    Update main.m

--- matlab/main.m
+++ matlab/main.m
@@ -4,7 +4,11 @@ clear;clc;
 aie_data_input = importdata("input.txt");
 FFT_data_in = aie_data_input(:,1)+aie_data_input(:,2)*i;
 FFT_out_matlab = fft(FFT_data_in);
-
+real_FFT_out_matlab=real(FFT_out0);
+imag_FFT_out_matlab=imag(FFT_out0);
+real_int16_FFT_out_matlab=aie_srs(real_FFT_out0, 0, 16);
+imag_int16_FFT_out_matlab=aie_srs(imag_FFT_out0, 0, 16);
+int16_FFT_out_matlab=real_int16_FFT_out_matlab+imag_int16_FFT_out_matlab*i;
 
 FFT_out_aieemu_txt = importdata("FFT_out_aieemu.txt");
 FFT_out_aieemu = FFT_out_aieemu_txt(:,1)+FFT_out_aieemu_txt(:,2)*i;
@@ -20,7 +24,7 @@ L_fft=length(FFT_data_in);
 subplot(1,2,1)
 title("matlab仿真结果") 
 hold on
-plot(1:length(FFT_out_matlab),abs(FFT_out_matlab));
+plot(1:length(int16_FFT_out_matlab),abs(int16_FFT_out_matlab));
 axis([0 L_fft 0 32768])
 
 subplot(1,2,2)
@@ -29,7 +33,7 @@ hold on
 plot(1:length(FFT_out_hardware),abs(FFT_out_hardware));
 axis([0 L_fft 0 32768])
 
-X=abs(FFT_out_matlab)-abs(FFT_out_hardware);
+X=abs(int16_FFT_out_matlab)-abs(FFT_out_hardware);
 
 figure(2)
 title("hardware compare with matlab")
